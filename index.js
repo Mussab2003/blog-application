0dacf4fc14cbd9dfead7753e3bd820235d386438
Mussab2003 <muhammadmussab12345@gmail.com>
2024-07-04 17:03:51 +0500

    .env v3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import 'dotenv/config'
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3333;
 console.log(port);
 var id;
 
